ac14fb2b52cb80f701b0f9f064846fa01d903fc6
Alexandr Zhilkin <azhilkin2033@gmail.com>
2016-12-23 13:32:42 +0300

    пытаюсь исправить работу SmartGit'a

--- src/main/java/erpsystem/model/Zhilkin_bean.java
+++ src/main/java/erpsystem/model/Zhilkin_bean.java
@@ -1,7 +1,15 @@
 package erpsystem.model;
 
+import org.springframework.stereotype.Controller;
+
 /**
  * Created by Alexandr Zhilkin on 23.12.2016.
  */
+@Controller
 public class Zhilkin_bean {
+    private String message = "Надеюсь я всё исправил.";
+
+    public String getMessage(){
+        return message;
+    }
 }
